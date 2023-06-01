fd6f71344207cb1facd7a78d27af0cecad3cf69a
Hugo <126873676+hugoarsa@users.noreply.github.com>
2023-06-01 11:57:58 +0200

    Update squares.pl
    lol

--- labo/segundo_parcial/lab6/squares.pl
+++ labo/segundo_parcial/lab6/squares.pl
@@ -10,6 +10,7 @@ ejemplo(4, 40,[24,16,16,10,9,8,8,7,7,6,6,3,3,3,2,1,1]).   %<-- aquest ja costa b
 %Aquests dos ultims son molt durs!!! Poden no sortir-te amb aquesta tècnica!!
 ejemplo(5,112,[50,42,37,35,33,29,27,25,24,19,18,17,16,15,11,9,8,7,6,4,2]).
 ejemplo(6,175,[81,64,56,55,51,43,39,38,35,33,31,30,29,20,18,16,14,9,8,5,4,3,2,1]).
+ejemplo(7,  3,[2]).
 
 
 %% Possible output solution for example 3:
@@ -34,20 +35,33 @@ ejemplo(6,175,[81,64,56,55,51,43,39,38,35,33,31,30,29,20,18,16,14,9,8,5,4,3,2,1]
 %%   3  3  3  3  3  3  1  1  1  1  3  3  3  3  3  3  3  3  3
 
 
-main:- 
-    ejemplo(3,Big,Sides),
+main:-
+    ejemplo(4,Big,Sides),
     nl, write('Fitting all squares of size '), write(Sides), write(' into big square of size '), write(Big), nl,nl,
-    length(Sides,N), 
+   
+    % Domain
+    length(Sides,N),
     length(RowVars,N), % get list of N prolog vars: Row coordinates of each small square
-    ...
-    insideBigSquare(N,Big,Sides,RowVars),
-    insideBigSquare(N,Big,Sides,ColVars),
+    length(ColVars,N),
+
+    limitaDomini(Big, Sides, RowVars),
+    limitaDomini(Big, Sides, ColVars),
+   
+    %Constraints
+
+    %insideBigSquare(N,Big,Sides,RowVars),
+    %insideBigSquare(N,Big,Sides,ColVars),
     nonoverlapping(N,Sides,RowVars,ColVars),
-    ...
+   
+    % Label
+    append(RowVars, ColVars, Vars),
+    labeling([down(Vars)], Vars),
+
+    % Print
     displaySol(Big,Sides,RowVars,ColVars), halt.
 
 
-displaySol(N,Sides,RowVars,ColVars):- 
+displaySol(N,Sides,RowVars,ColVars):-
     between(1,N,Row), nl, between(1,N,Col),
     nth1(K,Sides,S),    
     nth1(K,RowVars,RV),    RVS is RV+S-1,     between(RV,RVS,Row),
@@ -58,3 +72,25 @@ displaySol(_,_,_,_):- nl,nl,!.
 writeSide(S):- S<10, write('  '),write(S),!.
 writeSide(S):-       write(' ' ),write(S),!.
 
+insideBigSquare(_, _, [], []).
+insideBigSquare(N,Big,[S | Sides],[V | Vars]) :-
+    V + S -1 #=< Big,
+    insideBigSquare(N, Big, Sides, Vars).
+
+nonoverlapping(_, [], [], []).
+nonoverlapping(N,[S | Sides],[R | RowVars], [C | ColVars]) :-
+    oneWithAll(S, R, C, Sides, RowVars, ColVars),
+    nonoverlapping(N, Sides, RowVars, ColVars).
+
+oneWithAll(_, _, _, [], [], []).
+oneWithAll(S, R, C, [SN | Sides], [RN | RowVars], [CN | ColVars]) :-
+    (RN+SN -1 #< R #\/ RN #> R+S -1)
+        #\/
+    (CN+SN -1 #< C #\/ CN #> C+S -1),
+    oneWithAll(S, R, C, Sides, RowVars, ColVars).
+
+limitaDomini(_, [], []).
+limitaDomini(Big, [S | Sides], [C | Coord]) :-
+    Max is Big - S + 1,
+    C in 1..Max,
+    limitaDomini(Big, Sides, Coord).
